b2ab083490ab3b897e826555f3616cbc98a6071b
Serdar <serdar.basegmez@gmail.com>
2012-12-09 13:45:18 -0430

    Version 1.00

---
+++ DOTSFeedMonster/org.openntf.news.monster/src/org/openntf/news/shared/Utilities.java
@@ -0,0 +1,49 @@
+package org.openntf.news.shared;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Random;
+
+public final class Utilities {
+
+	private static String UNIQUEID_LETTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static int UNIQUEID_DEFAULTSIZE = 6;
+	
+	public static String generateUniqueId() {
+		StringBuilder sb = new StringBuilder(UNIQUEID_DEFAULTSIZE);
+		Random random = new Random();
+		for (int i=0; i < UNIQUEID_DEFAULTSIZE; i++) {
+			sb.append(UNIQUEID_LETTERS.charAt(random.nextInt(UNIQUEID_LETTERS.length())));
+		}
+	    return sb.toString().toLowerCase(Locale.US);            
+	}
+
+	public static String followRedirect(String urlString) throws IOException {
+		URL url=new URL(urlString);
+		HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+		conn.setInstanceFollowRedirects(false);
+
+		if(conn.getResponseCode()==301 && conn.getHeaderField("Location")!=null) {
+			return conn.getHeaderField("Location");
+		}
+		
+		return urlString;
+	}
+	
+	public static String getMemoryStatus() {
+		Runtime rt = Runtime.getRuntime();
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setGroupingUsed(true);
+		nf.setMinimumFractionDigits(0);
+		long total_mem = rt.totalMemory();
+		long free_mem = rt.freeMemory();
+		long used_mem = total_mem - free_mem;
+		return "Amount of used memory/free memory: " + nf.format(used_mem/1000) + "KB / " + nf.format(free_mem/1000)+" KB";
+		
+		
+	}
+	
+}
