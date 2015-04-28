47b3d08fbf05be2e84f940e5d0edba50fa572caf
Oliver Busse <oliver.busse@we4it.com>
2015-04-29 01:03:13 +0200

    Landing page

--- ODP/Code/Java/org/openntf/ui/Page.java
+++ ODP/Code/Java/org/openntf/ui/Page.java
@@ -3,10 +3,28 @@ package org.openntf.ui;
 import java.io.Serializable;
 
 public class Page implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -810514190365137779L;
+	private String label;
+	private String url;
+	private String icon;
 
+	public Page(String label, String icon, String url) {
+		this.label = label;
+		this.icon = icon;
+		this.url = url;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+	
+	
 }
