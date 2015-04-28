9ddccaa792528577edac79b9fd4e065dbaf99fb5
Oliver Busse <oliver.busse@we4it.com>
2015-04-29 00:38:44 +0200

    BS3 layout CC, navigator classes

---
+++ ODP/Code/Java/org/openntf/ui/Navigation.java
@@ -0,0 +1,12 @@
+package org.openntf.ui;
+
+import java.io.Serializable;
+
+public class Navigation implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4983110583704441736L;
+
+}
---
+++ ODP/Code/Java/org/openntf/ui/Page.java
@@ -0,0 +1,12 @@
+package org.openntf.ui;
+
+import java.io.Serializable;
+
+public class Page implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -810514190365137779L;
+
+}
