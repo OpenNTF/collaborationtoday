3ec7fe240deb1d5c670cb037c45a704e6e9f3b08
Oliver Busse <oliver.busse@we4it.com>
2015-04-29 22:39:07 +0200

    Dynamic navigation for main pages

--- ODP/Code/Java/org/openntf/ui/Navigation.java
+++ ODP/Code/Java/org/openntf/ui/Navigation.java
@@ -1,12 +1,24 @@
 package org.openntf.ui;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Navigation implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -4983110583704441736L;
+	private List<Page> navigation;
+	
+	public Navigation(){
+		navigation = new ArrayList<Page>();
+		navigation.add(new Page("Home", "", "home.xsp"));
+		navigation.add(new Page("Follow", "", "follow.xsp"));
+		navigation.add(new Page("Contact", "", "contact.xsp"));
+		navigation.add(new Page("About", "", "about.xsp"));
+	}
+
+	public List<Page> getNavigation() {
+		return navigation;
+	}
 
 }
