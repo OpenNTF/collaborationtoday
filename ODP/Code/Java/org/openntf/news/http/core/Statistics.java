4b08b09dbd510ceee9a2dbf36828008bd964dc52
Oliver Busse <oliver.busse@we4it.com>
2016-03-29 15:41:48 +0200

    fixed missing property in xsp config file

--- ODP/Code/Java/org/openntf/news/http/core/Statistics.java
+++ ODP/Code/Java/org/openntf/news/http/core/Statistics.java
@@ -22,20 +22,27 @@ public class Statistics implements Serializable {
 	private static final String ITEM_EMAIL = "PEMail";
 	private static final String ITEM_PICTURE_URL = "PPictureURL";
 	private static final String ITEM_PICTURE_TYPE = "PPictureType";
+	private static final String ITEM_PID = "PID";
 
 	private List<Person> authors;
-	private List<String> curators;
+	private List<Person> curators;
 
 	public Statistics() {
 		authors = new ArrayList<Person>();
-		curators = new ArrayList<String>();
+		curators = new ArrayList<Person>();
 
+		init();
+	}
+
+	private void init() {
 		try {
 			// views
 			View personsById = ExtLibUtil.getCurrentDatabase().getView("PersonsByPID");
 
 			// get all posts and their amounts by poster
 			View newsByAuthor = ExtLibUtil.getCurrentDatabase().getView("NewsByAuthor");
+			View newsByCurator = ExtLibUtil.getCurrentDatabase().getView("NewsByCurator");
+			View personsAll = ExtLibUtil.getCurrentDatabase().getView("PersonsAll");
 			ViewNavigator nav = newsByAuthor.createViewNav();
 			ViewEntry ent = nav.getFirst();
 			while (ent != null) {
@@ -53,6 +60,28 @@ public class Statistics implements Serializable {
 				ent = nav.getNextCategory();
 			}
 
+			nav = newsByCurator.createViewNav();
+			ent = nav.getFirst();
+			while (ent != null) {
+				String curatorDisplayName = ent.getColumnValues().elementAt(0).toString();
+				if (!curatorDisplayName.isEmpty()) {
+					// trying to guess the PID of the curator
+					Document curator = personsAll.getDocumentByKey(curatorDisplayName);
+					String pid = "";
+					if (curator != null) {
+						pid = curator.getItemValueString(ITEM_PID);
+						double posts = Double.valueOf(ent.getColumnValues().elementAt(1).toString());
+						Document pDoc = personsById.getDocumentByKey(pid);
+						if (pDoc != null) {
+							Person person = new Person(posts, pid, pDoc.getItemValueString(ITEM_DISPLAY_NAME), pDoc.getItemValueString(ITEM_TWITTER), pDoc.getItemValueString(ITEM_EMAIL), pDoc
+									.getItemValueString(ITEM_PICTURE_TYPE), pDoc.getItemValueString(ITEM_PICTURE_URL), pDoc.getUniversalID());
+							curators.add(person);
+						}
+					}
+				}
+				ent = nav.getNextCategory();
+			}
+
 		} catch (NotesException e) {
 			e.printStackTrace();
 		}
@@ -63,7 +92,8 @@ public class Statistics implements Serializable {
 		return authors;
 	}
 
-	public List<String> getCurators() {
+	public List<Person> getCurators() {
+		Collections.sort(curators);
 		return curators;
 	}
 
